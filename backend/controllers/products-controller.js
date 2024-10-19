bb7b06a7c696239e9b96de28d166720537f71e5c
labib-islam <labibislam87@gmail.com>
2024-10-19 16:38:44 +0600

    backend: CRUD product

--- backend/controllers/products-controller.js
+++ backend/controllers/products-controller.js
@@ -14,9 +14,78 @@ export const createProduct = (req, res) => {
 
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err)
-    return res.status(200).json({ 
-      message: 'Product has been created.', 
+    return res.status(200).json({
+      message: 'Product has been created.',
       data: data[0]
     });
+  })
+}
+
+
+export const getProducts = (req, res) => {
+  const q = "SELECT * FROM products"
+
+  db.query(q, (err, data) => {
+    if (err) return res.json(err)
+    return res.status(200).json(data);
+  })
+}
+
+
+export const getProduct = (req, res) => {
+  const q = "SELECT * FROM products WHERE pid=?"
+
+  db.query(q, [req.params.pid], (err, data) => {
+    if (err) return res.json(err)
+    return res.status(200).json(data);
+  })
+}
+
+
+export const updateProduct = (req, res) => {
+  const q = "SELECT * FROM products WHERE pid=?"
+
+  db.query(q, [req.params.pid], (err, data) => {
+    if (err) return res.json(err)
+    if (data.length === 0) return res.status(404).json('Product not found!')
+    const product = data[0]
+    if (product.uid !== req.uid) return res.status(401).json('You cannot update the product.')
+
+    const q = "UPDATE products SET title=?, description=?, price=?, stock=? WHERE pid=?"
+
+    const values = [
+      req.body.title,
+      req.body.description,
+      req.body.price,
+      req.body.stock,
+      req.params.pid
+    ]
+
+    db.query(q, values, (err, data) => {
+      if (err) return res.json(err)
+      return res.status(200).json({
+        message: 'Product has been updated.',
+        data: data[0]
+      });
+    })
+  })
+}
+
+
+export const deleteProduct = (req, res) => {
+  const q = "SELECT * FROM products WHERE pid=?"
+
+  db.query(q, [req.params.pid], (err, data) => {
+    if (err) return res.json(err)
+    if (data.length === 0) return res.status(404).json('Product not found!')
+    const product = data[0]
+    if (product.uid !== req.uid) return res.status(401).json('You cannot delete the product.')
+
+    const q = "DELETE FROM products WHERE pid=?"
+
+    db.query(q, [req.params.pid], (err, data) => {
+      if (err) return res.json(err)
+      return res.status(200).json('Product has been deleted.');
+    })
   })
 }
--- backend/routes/products-routes.js
+++ backend/routes/products-routes.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import auth from '../middlewares/auth.js'
-import { createProduct } from '../controllers/products-controller.js'
+import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from '../controllers/products-controller.js'
 import fileUpload from '../middlewares/file-upload.js';
 
 const router = express.Router();
 
 router.post('/new', auth, fileUpload.single('image'), createProduct)
+
+router.get('/', getProducts)
+router.get('/:pid', getProduct)
+router.put('/:pid', auth, updateProduct)
+router.delete('/:pid', auth, deleteProduct)
 
 export default router
