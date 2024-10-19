968ca955d67a3a2971bc4822d14f5f6fa82dc236
labib-islam <labibislam87@gmail.com>
2024-10-19 17:22:16 +0600

    backend: hash password using callback

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -11,21 +11,21 @@ export const register = (req, res) => {
     if (data.length) return res.status(409).json('User already exists!')
 
     // Hash password and create user
-    const salt = bcrypt.genSalt(10);
-    const hashedPassword = bcrypt.hash(req.body.password, salt);
-
-    const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)"
-    const values = [
-      req.body.username,
-      req.body.email,
-      hashedPassword,
-    ]
-
-    db.query(q, [values], (err, data) => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
       if (err) return res.json(err)
-      return res.status(200).json('User has been created.');
-    })
 
+      const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)"
+      const values = [
+        req.body.username,
+        req.body.email,
+        hash,
+      ]
+
+      db.query(q, [values], (err, data) => {
+        if (err) return res.json(err)
+        return res.status(200).json('User has been created.');
+      })
+    });
   })
 
 }
@@ -40,17 +40,17 @@ export const login = (req, res) => {
     if (data.length === 0) return res.status(404).json('User not found!')
 
     // Check password
-    const isPasswordCorrect = bcrypt.compare(req.body.password, data[0].password)
+    bcrypt.compare(req.body.password, data[0].password, (err, result) => {
+      if (err) return res.json(err)
+      if (!result) return res.status(400).json('Wrong username or password.');
 
-    if (!isPasswordCorrect) return res.status(400).json('Wrong username or password.');
-
-    const token = jwt.sign({ uid: data[0].uid }, process.env.JWT_KEY)
-    const { password, ...other } = data[0]
-
-    res.cookie('access_token', token, {
-      httpOnly: true
-    }).status(200).json(other)
-
+      const token = jwt.sign({ uid: data[0].uid }, process.env.JWT_KEY)
+      const { password, ...other } = data[0]
+  
+      res.cookie('access_token', token, {
+        httpOnly: true
+      }).status(200).json(other)
+    })
   })
 }
 
